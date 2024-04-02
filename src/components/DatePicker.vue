ff9ac1bae42e9ab29cd767d1ecaa69f4143ed68b
Tereza Buchníčková <xbuchni2@mendelu.cz>
2024-04-02 12:32:54 +0200

    DatePicker.vue

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-date-picker
+        v-model="selectedDate"
+        locale="cs"
+        :min="minDate"
+        :max="maxDate"
+        color="#65a61a"
+    ></v-date-picker>
+</template>
+
+<script>
+export default {
+    props: {
+        minDate: {
+            type: String,
+            default: null
+        },
+        maxDate: {
+            type: String,
+            default: null
+        }
+    },
+    data() {
+        return {
+            selectedDate: null
+        };
+    }
+};
+</script>
+
+<style scoped>
+</style>
